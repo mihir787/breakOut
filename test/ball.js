f6ed68e084a35c6f2a472750cdf1f8abd2ed1a2f
Kyle Henson <khenson.kh@gmail.com>
2015-07-21 14:27:57 -0600

    Started test for bounce of ball

---
+++ test/ball.js
@@ -0,0 +1,29 @@
+const assert = require('chai').assert;
+const Ball = require('../lib/ball');
+
+describe('Ball', function () {
+  it('should work', function () {
+    assert(Ball);
+  });
+
+  it('should construct an object', function() {
+    assert(new Ball());
+  });
+
+  it('should bounce', function() {
+    const ball = new Ball();
+    var width = 300;
+    var height = 300;
+    var x = 150;
+    var y = 150;
+    var horizontalSlope = 2;
+    var verticalSlope = 4;
+    assert.equal()
+  });
+
+  it.skip('should have properties including color and a circular shape', function() {
+    const ball = new Ball();
+    assert.equal(ball.size,  "75, 75, 10, 0, Math.PI*2, true")
+    assert.equal(ball.color,  "red")
+  });
+});
